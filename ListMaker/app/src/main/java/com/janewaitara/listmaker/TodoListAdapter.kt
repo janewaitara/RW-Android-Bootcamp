a81fd16a4bc5d8b0a546285a2b5bb4001c05b6af
janewaitara <janewaitara99@gmail.com>
2020-06-17 00:02:32 +0300

    Added a RecyclerView , its Adapter and a ViewHolder

---
+++ ListMaker/app/src/main/java/com/janewaitara/listmaker/TodoListAdapter.kt
@@ -0,0 +1,18 @@
+package com.janewaitara.listmaker
+
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoListAdapter: RecyclerView.Adapter<TodoListViewHolder> (){
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TodoListViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: TodoListViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+}
---
+++ ListMaker/app/src/main/java/com/janewaitara/listmaker/TodoListViewHolder.kt
@@ -0,0 +1,9 @@
+package com.janewaitara.listmaker
+
+import android.view.View
+import androidx.recyclerview.widget.RecyclerView
+
+class TodoListViewHolder(itemView: View): RecyclerView.ViewHolder(itemView) {
+
+
+}
