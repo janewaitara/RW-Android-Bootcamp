0e448b8ad9098cb9ff2b6b6993b0e33c42430f8e
janewaitara <janewaitara99@gmail.com>
2020-06-21 04:43:32 +0300

    Added a Recycler View

---
+++ Creatures/app/src/main/java/com/raywenderlich/android/creatures/app/ViewGroupExtensions.kt
@@ -0,0 +1,11 @@
+package com.raywenderlich.android.creatures.app
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.annotation.LayoutRes
+
+fun ViewGroup.inflate(@LayoutRes layoutRes: Int, attachToRoot: Boolean): View {
+    return LayoutInflater.from(context).inflate(layoutRes, this, attachToRoot)
+
+}
