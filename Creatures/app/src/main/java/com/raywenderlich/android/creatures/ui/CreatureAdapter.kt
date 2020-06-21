e2660656adf351ef1c38e97933b3dd652e741037
janewaitara <janewaitara99@gmail.com>
2020-06-21 04:55:43 +0300

    Bound the data with the RecyclerView

--- Creatures/app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
+++ Creatures/app/src/main/java/com/raywenderlich/android/creatures/ui/CreatureAdapter.kt
@@ -6,6 +6,7 @@ import androidx.recyclerview.widget.RecyclerView
 import com.raywenderlich.android.creatures.R
 import com.raywenderlich.android.creatures.app.inflate
 import com.raywenderlich.android.creatures.model.Creature
+import kotlinx.android.synthetic.main.list_item_creatures.view.*
 
 class CreatureAdapter(private val creatures: List<Creature>): RecyclerView.Adapter<CreatureAdapter.ViewHolder>() {
 
@@ -18,10 +19,20 @@ class CreatureAdapter(private val creatures: List<Creature>): RecyclerView.Adapt
     override fun getItemCount() = creatures.size
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bind(creatures[position])
 
     }
 
     class ViewHolder(itemView: View): RecyclerView.ViewHolder(itemView){
+        private lateinit var creature: Creature
 
+        fun bind(creature: Creature){
+           this.creature = creature
+            val context = itemView.context
+            //set the creature image and fullName using data from the creature
+            itemView.creatureImage.setImageResource(context.resources.getIdentifier(creature.uri, null,context.packageName))
+            itemView.fullName.text = creature.fullName
+
+        }
     }
 }
