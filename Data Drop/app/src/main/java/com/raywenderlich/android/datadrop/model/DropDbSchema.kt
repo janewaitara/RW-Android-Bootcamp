c75e5253d73b54378e2b1747cfb12e68b99898c6
janewaitara <janewaitara99@gmail.com>
2020-06-27 01:22:43 +0300

    Created an SQLite Database: Added a DbSchema to define the sctructure of the db, added an openHelper to create the database and a SQLite Repository that implements the DropRepository interface

---
+++ Data Drop/app/src/main/java/com/raywenderlich/android/datadrop/model/DropDbSchema.kt
@@ -0,0 +1,19 @@
+package com.raywenderlich.android.datadrop.model
+
+//defines the structure of the db
+object DropDbSchema {
+
+    const val VERSION = 1
+    const val DB_NAME = "drops.db"
+
+    object DropTable{
+        const val  NAME = "drops"
+
+        object Columns{
+            const val ID = "id"
+            const val LATITUDE = "latitude"
+            const val LONGITUDE = "longitude"
+            const val DROP_MESSAGE = "dropMessage"
+        }
+    }
+}
