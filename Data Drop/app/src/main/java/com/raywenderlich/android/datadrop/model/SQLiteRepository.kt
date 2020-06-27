1e80cd29bc03202e75d29e088a03bd96216cc969
janewaitara <janewaitara99@gmail.com>
2020-06-27 03:24:58 +0300

    Added a function that put data into contentValues and return the contentValues and added logic to insert the data into the database

--- Data Drop/app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
+++ Data Drop/app/src/main/java/com/raywenderlich/android/datadrop/model/SQLiteRepository.kt
@@ -1,13 +1,17 @@
 package com.raywenderlich.android.datadrop.model
 
+import android.content.ContentValues
 import com.raywenderlich.android.datadrop.app.DataDropApplication
+import com.raywenderlich.android.datadrop.model.DropDbSchema.DropTable
 
 class SQLiteRepository : DropRepository {
 
     private val database = DropDbHelper(DataDropApplication.getAppContext()).writableDatabase
 
     override fun addDrop(drop: Drop) {
-
+        //get a contentValue object from the drop an use insert method in the db to insert the drop
+        val contentValues = getDropContentValues(drop)
+        database.insert(DropTable.NAME,null,contentValues)
     }
 
     override fun getDrops(): List<Drop> {
@@ -20,5 +24,10 @@ class SQLiteRepository : DropRepository {
 
     override fun clearAllDrops() {
 
+    }
+    private fun getDropContentValues(drop: Drop): ContentValues{
+        val contentValues = ContentValues()
+        contentValues.put(DropTable.Columns.ID, drop.id)
+        return contentValues
     }
 }
