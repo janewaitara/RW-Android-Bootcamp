4bcbaa9c08a931105568ed8ad5fb96342a88d030
janewaitara <janewaitara99@gmail.com>
2020-07-25 01:10:45 +0300

    Implemented a Room database and a room repository

--- MVVM on android/start/app/src/main/java/com/raywenderlich/android/creaturemon/app/CreaturemonApplication.kt
+++ MVVM on android/start/app/src/main/java/com/raywenderlich/android/creaturemon/app/CreaturemonApplication.kt
@@ -31,6 +31,8 @@
 package com.raywenderlich.android.creaturemon.app
 
 import android.app.Application
+import android.arch.persistence.room.Room
+import android.arch.persistence.room.RoomDatabase
 import com.raywenderlich.android.creaturemon.model.room.CreatureDatabase
 
 class CreaturemonApplication : Application() {
@@ -41,6 +43,8 @@ class CreaturemonApplication : Application() {
 
   override fun onCreate() {
     super.onCreate()
-    // TODO: init database
+
+    database = Room.databaseBuilder(this, CreatureDatabase::class.java, "creature_database")
+            .build()
   }
 }
--- MVVM on android/start/app/src/main/java/com/raywenderlich/android/creaturemon/model/room/RoomRepository.kt
+++ MVVM on android/start/app/src/main/java/com/raywenderlich/android/creaturemon/model/room/RoomRepository.kt
@@ -30,6 +30,7 @@
 
 package com.raywenderlich.android.creaturemon.model.room
 
+import android.arch.lifecycle.LiveData
 import android.os.AsyncTask
 import com.raywenderlich.android.creaturemon.app.CreaturemonApplication
 import com.raywenderlich.android.creaturemon.model.Creature
@@ -38,15 +39,37 @@ import com.raywenderlich.android.creaturemon.model.CreatureRepository
 class RoomRepository : CreatureRepository {
   private val creatureDao: CreatureDao = CreaturemonApplication.database.creatureDao()
 
+  //asks the dao for all creatures
+  private val allCreatures: LiveData<List<Creature>>
+
+  init {
+      allCreatures = creatureDao.getAllCretures()
+  }
+
   private class InsertAsyncTask internal constructor(private val dao: CreatureDao) : AsyncTask<Creature, Void, Void>() {
     override fun doInBackground(vararg params: Creature): Void? {
+      dao.insert(params[0])
       return null
     }
   }
 
   private class DeleteAsyncTask internal constructor(private val dao: CreatureDao) : AsyncTask<Creature, Void, Void>() {
     override fun doInBackground(vararg params: Creature): Void? {
+      dao.clearCreatures(*params)
       return null
+    }
+  }
+
+  override fun saveCreature(creature: Creature) {
+    InsertAsyncTask(creatureDao).execute(creature)
+  }
+
+  override fun getAllCreatures(): LiveData<List<Creature>>  = allCreatures
+
+  override fun clearAllCreatures() {
+    val creatureArray = allCreatures.value?.toTypedArray()
+    if (creatureArray != null) {
+      DeleteAsyncTask(creatureDao).execute(*creatureArray)
     }
   }
 }
