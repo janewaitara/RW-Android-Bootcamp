f0f3d9b32de292a619b1e1cd12870e7cb9cb92af
janewaitara <janewaitara99@gmail.com>
2020-07-30 15:02:51 +0300

    Added a base URl in the networkModule

--- SpaceDaily/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ SpaceDaily/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -4,15 +4,30 @@ import kotlinx.coroutines.Dispatchers
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
 
+
+/**
+sample
+
 val networkModule = module {
-    //Creates only one class no matter the times it is called
-    single(named("name")){
-    }
-    //factory creates a new instance each time a variable is defined
+//Creates only one class no matter the times it is called
+single(named("name")){
+}
+//factory creates a new instance each time a variable is defined
 }
 
 val coroutines = module {
-    single(named("Background")) { Dispatchers.Default }
-    single(named("IO")) { Dispatchers.IO }
-    single(named("UI")) { Dispatchers.Main }
+single(named("Background")) { Dispatchers.Default }
+single(named("IO")) { Dispatchers.IO }
+single(named("UI")) { Dispatchers.Main }
+
+factory (named("name")){
+
+}
+}*/
+
+
+val networkModule = module {
+    single (named("BASE_URL")){
+        "https://api.nasa.gov/plantary"
+    }
 }
