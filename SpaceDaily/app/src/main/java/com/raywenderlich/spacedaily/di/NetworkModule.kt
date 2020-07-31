e5a0a34e5f1044bc7239051d951fc70f06a8b295
janewaitara <janewaitara99@gmail.com>
2020-07-31 19:26:59 +0300

    Added a network Module with several singles for the network builder

--- SpaceDaily/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
+++ SpaceDaily/app/src/main/java/com/raywenderlich/spacedaily/di/NetworkModule.kt
@@ -1,8 +1,17 @@
 package com.raywenderlich.spacedaily.di
 
+import com.raywenderlich.spacedaily.network.NASAAPIInterface
+import com.squareup.moshi.Moshi
+import com.squareup.moshi.kotlin.reflect.KotlinJsonAdapterFactory
 import kotlinx.coroutines.Dispatchers
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
+import org.koin.android.BuildConfig
 import org.koin.core.qualifier.named
 import org.koin.dsl.module
+import retrofit2.Retrofit
+import retrofit2.converter.moshi.MoshiConverterFactory
+import java.util.concurrent.TimeUnit
 
 
 /**
@@ -25,9 +34,43 @@ factory (named("name")){
 }
 }*/
 
-
+//building all variables to inject
 val networkModule = module {
     single (named("BASE_URL")){
         "https://api.nasa.gov/plantary"
+    }
+    //Logging Interceptor
+    single {
+        val interceptor = HttpLoggingInterceptor()
+        interceptor.level = HttpLoggingInterceptor.Level.BODY
+        interceptor //returning the interceptor
+    }
+    //OKHTTPClient class
+    single {
+        val clientBuilder = OkHttpClient().newBuilder()
+            .connectTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS)
+            .readTimeout(30, TimeUnit.SECONDS)
+        if ( BuildConfig.DEBUG){
+            clientBuilder.addInterceptor(get<HttpLoggingInterceptor>()) //using get with a type to let koin know which interceptor to get
+        }
+        clientBuilder.build()
+    }
+    //moshi instance
+    single {
+        Moshi.Builder()
+            .add(KotlinJsonAdapterFactory())//adapts json so that it can convert any json into our model object
+            .build()
+    }
+    //Retrofit class
+    single {
+        Retrofit.Builder().baseUrl(get<String>(named("BASE_URL")))
+            .addConverterFactory(MoshiConverterFactory.create(get()))
+            .client(get())
+            .build()
+    }
+    //create NASA interface
+    single {
+        get<Retrofit>().create(NASAAPIInterface::class.java)
     }
 }
