95674452367535ac1a78e1b8a29efe9ff3b81367
janewaitara <janewaitara99@gmail.com>
2020-08-09 18:49:29 +0300

    Added a flingAnimation to the donut in checkoutActivity

--- FoodMap/app/src/main/java/com/raywenderlich/android/foodmart/ui/checkout/CheckoutActivity.kt
+++ FoodMap/app/src/main/java/com/raywenderlich/android/foodmart/ui/checkout/CheckoutActivity.kt
@@ -31,20 +31,32 @@
 
 package com.raywenderlich.android.foodmart.ui.checkout
 
+import android.animation.ObjectAnimator
 import android.content.Context
 import android.content.Intent
+import android.graphics.Rect
 import android.os.Bundle
 import android.support.animation.DynamicAnimation
+import android.support.animation.FlingAnimation
 import android.support.animation.SpringAnimation
 import android.support.animation.SpringForce
 import android.support.v7.app.AppCompatActivity
+import android.util.DisplayMetrics
+import android.view.GestureDetector
 import android.view.MotionEvent
+import android.view.View
+import android.view.ViewTreeObserver
+import android.view.animation.AccelerateDecelerateInterpolator
 import com.raywenderlich.android.foodmart.R
+import com.raywenderlich.android.foodmart.app.toast
 import kotlinx.android.synthetic.main.activity_checkout.*
 
 class CheckoutActivity : AppCompatActivity() {
 
-  //property to measure the x displacement
+  /**
+   * Physics Based animation:
+   *    Spring Animation*/
+  //property to measure the x and y displacement
   private var xPositionDiff = 0f
   private var yPositionDiff = 0f
 
@@ -55,9 +67,6 @@ class CheckoutActivity : AppCompatActivity() {
       dampingRatio = SpringForce.DAMPING_RATIO_HIGH_BOUNCY
     }
   }
-  /**
-   * Physics Based animation:
-   *    Spring Animation*/
   //Spring Animation constructor takes an object to act on and an associated property
   // which in this case is translationX for horizontal displacement
   private val springAnimationX: SpringAnimation by lazy {
@@ -66,19 +75,6 @@ class CheckoutActivity : AppCompatActivity() {
 
   private val springAnimationY : SpringAnimation by lazy {
     SpringAnimation(donut, DynamicAnimation.TRANSLATION_Y).setSpring(springForce)
-  }
-
-  companion object {
-    fun newIntent(context: Context) = Intent(context, CheckoutActivity::class.java)
-  }
-
-  override fun onCreate(savedInstanceState: Bundle?) {
-    super.onCreate(savedInstanceState)
-    setContentView(R.layout.activity_checkout)
-
-    title = getString(R.string.checkout_title)
-
-    setupTouchListener()
   }
 
   //respond to a user's touch to start the animation by setting up a touch listener on the donut
@@ -109,4 +105,119 @@ class CheckoutActivity : AppCompatActivity() {
       true
     }
   }
+
+  /**
+   * Physics Based animation:
+   *    Fling Animation*/
+  private var donutFlingCount = 0f
+
+  //gestureListener for the donut
+  private val donutGestureListener = object: GestureDetector.SimpleOnGestureListener(){
+    override fun onDown(e: MotionEvent?) = true
+
+    override fun onFling(e1: MotionEvent?, e2: MotionEvent?, velocityX: Float, velocityY: Float): Boolean {
+      //limiting the user to one fling
+      if (donutFlingCount < 1){
+        donutFlingAnimationX.setStartVelocity(velocityX)
+        donutFlingAnimationY.setStartVelocity(velocityY)
+
+        donutFlingAnimationX.start()
+        donutFlingAnimationY.start()
+      }
+      return true
+    }
+  }
+
+  private val donutGestureDetector: GestureDetector by lazy {
+    GestureDetector(this, donutGestureListener)
+  }
+
+  private val donutFlingAnimationX: FlingAnimation by lazy {
+   FlingAnimation(donut, DynamicAnimation.X).setFriction(1f)
+  }
+  private val donutFlingAnimationY : FlingAnimation by lazy {
+    FlingAnimation(donut, DynamicAnimation.Y).setFriction(1f)
+  }
+
+  //animating the block to move back and forth
+  private fun setUpAnimatingBlock(){
+    val displayMetrics = DisplayMetrics()
+    windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+    val width = displayMetrics.widthPixels
+
+    ObjectAnimator.ofFloat(block, "translationX", 0f,
+            width.toFloat()-resources.getDimension(R.dimen.block_width)).apply {
+      interpolator = AccelerateDecelerateInterpolator()
+      duration = 1000L
+      repeatCount = ObjectAnimator.INFINITE
+      repeatMode = ObjectAnimator.REVERSE
+      start()
+    }
+  }
+
+  //limiting the motion of the donut to stay in the view by adding a globalLayoutListener to the donut viewTreeObserver
+  private fun setupTreeObserver(){
+    donut.viewTreeObserver.addOnGlobalLayoutListener(object : ViewTreeObserver.OnGlobalLayoutListener{
+      override fun onGlobalLayout() {
+        val displayMetrics = DisplayMetrics()
+        windowManager.defaultDisplay.getMetrics(displayMetrics)
+
+        val width = displayMetrics.widthPixels
+        val height = displayMetrics.heightPixels
+        donutFlingAnimationX.setMinValue(0f).setMaxValue((width - donut.width).toFloat())
+        donutFlingAnimationY.setMinValue(0f).setMaxValue((height - 2 * donut.height).toFloat())
+
+        donut.viewTreeObserver.removeOnGlobalLayoutListener(this)
+
+      }
+    })
+  }
+
+  //helper method to determine if two views are overlapping by checking if the corresponding HitRects intersect
+  private fun isViewOverLapping(v1: View, v2: View): Boolean{
+    val rect1 = Rect()
+    v1.getHitRect(rect1)
+
+    val rect2 = Rect()
+    v2.getHitRect(rect2)
+
+    return Rect.intersects(rect1,rect2)
+  }
+
+  //helper method to increment the flingCount and cast for a successful fling when the flingAnimation ends
+  private fun setUpEndListener(){
+    donutFlingAnimationX.addEndListener { _, _, _, _ ->
+      donutFlingCount += 1
+      //if the donut overlaps the block alert by a toast
+      if (isViewOverLapping(donut,block)){
+        toast(getString(R.string.free_donuts))
+      }
+    }
+  }
+
+  //passing touches on the donut to our gestureDetector
+  private fun setupTouchListenerByFling(){
+    donut.setOnTouchListener { _, motionEvent ->
+      donutGestureDetector.onTouchEvent(motionEvent)
+    }
+  }
+
+  companion object {
+    fun newIntent(context: Context) = Intent(context, CheckoutActivity::class.java)
+  }
+
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    setContentView(R.layout.activity_checkout)
+
+    title = getString(R.string.checkout_title)
+
+    setUpAnimatingBlock()
+    setupTouchListenerByFling()
+    setupTreeObserver()
+    setUpEndListener()
+  }
+
+
 }
